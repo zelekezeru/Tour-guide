a169892cd8dde79c9a2694998d64b43768686c41
Abi Zeleke <zelekezeru@gmail.com>
2024-07-25 16:18:11 +0300

    Resource Controller made
    -Resource controller created
    - main template pages imported
    - web route designed
    - navigation moved to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -57,7 +57,42 @@
 	</head>
 	<body>
         <div class="colorlib-loader"></div>
+        <nav class="colorlib-nav" role="navigation">
+            <div class="top-menu">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <div id="colorlib-logo"><a href="index.html">AlphaTour</a></div>
+                        </div>
+                        <div class="col-xs-10 text-right menu-1">
+                            <ul>
+                                <li class="active"><a href="index.html">Home</a></li>
+                                <li class="has-dropdown">
+                                    <a href="tours.html">Tours</a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">Destination</a></li>
+                                        <li><a href="#">Cruises</a></li>
+                                        <li><a href="#">Hotels</a></li>
+                                        <li><a href="#">Booking</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="hotels.html">Hotels</a></li>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+                                {{-- Main Page Contents go here --}}
+                                
         {{ $slot }}
+
+
         <div id="page">
             <footer id="colorlib-footer" role="contentinfo">
                 <div class="container">
@@ -133,7 +168,7 @@
         </div>
         <div class="gototop js-top">
             <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
-	</div>
+	    </div>
 	
 	<!-- jQuery -->
 	<script src="{{ asset('js/jquery.min.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
@@ -18,3 +18,15 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::resource('abouts', AboutController::class);
+
+Route::resource('blogs', BlogController::class);
+
+Route::resource('contacts', ContactController::class);
+
+Route::resource('homes', HomeController::class);
+
+Route::resource('hotels', HotelController::class);
+
+Route::resource('tours', TourController::class);
