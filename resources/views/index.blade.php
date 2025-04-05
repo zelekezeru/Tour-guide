abae5d954593be2d1fb1e6fe560e9c5d9d901974
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-04-05 09:37:41 +0300

    Update index.blade.php
    fixed a bug with testimonials not showing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -364,7 +364,7 @@
             </div>
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 animate-box">
-                    <div class="owl-carousel2">
+                    <div class="owl-carousel">
 
                         @foreach($testimonials as $testimonial)
 
