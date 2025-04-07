142973f032aba0f9433270dda254b998e5013a62
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-04-07 11:57:20 +0300

    Update guest-nav.blade.php
    -fixed logout not showing in dropdown

--- resources/views/layouts/guest-nav.blade.php
+++ resources/views/layouts/guest-nav.blade.php
@@ -21,23 +21,19 @@
                         <li><a href="{{ route('blogs.index') }}">Blog</a></li>
                         <li><a href="{{ route('abouts.index') }}">About</a></li>
                         <li><a href="{{ route('contacts.index') }}">Contact</a></li>
-
-                        <li class="nav-item dropdown no-arrow bg-dark">
+                        <li class="has-dropdown">
                             @if( Auth::user() )
                                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <img class="img-profile rounded-circle" src="{{ asset( 'images/userIcon.png' ) }}" style="max-width: 60px">
                                         <span class="ml-2 d-none d-lg-inline text-white small">{{ Auth::user()->name }}</span>
                                     </a>
-                                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in bg-dark" aria-labelledby="userDropdown">
+                                    <div class="dropdown">
 
-                                        <a class="dropdown-item text-white" href="#">
-                                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile
+                                        <a class="" href="#">
+                                            <i class="fas fa-user fa-sm fa-fw mr-2"></i> Profile
                                         </a>
-                                        <a class="dropdown-item text-white" href="{{ route('admin.dashboard') }}">
-                                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Dashbord
-                                        </a>
-                                        <a class="dropdown-item text-white" href="#">
-                                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log
+                                        <a class="" href="{{ route('admin.dashboard') }}">
+                                            <i class="fas fa-cogs fa-sm fa-fw mr-2"></i> Dashbord
                                         </a>
                                         <div class="dropdown-divider"></div>
                                         <!-- Authentication -->
